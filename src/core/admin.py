c4bfebd9c4998c04fcdd46e7a7857519361fd337
Сотниченко Алексей Дмитриевич <a.sotnichenko@els24.com>
2025-02-12 09:59:10 +0700

    + model and register it to admin panel

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Catalog)
+class CatalogAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(VersionCatalog)
+class VersionCatalogAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ElementCatalog)
+class ElementCatalogAdmin(admin.ModelAdmin):
+    pass
--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,56 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+__all__ = ['Catalog', 'VersionCatalog', 'ElementCatalog']
+
+
+class BaseModel(models.Model):
+    """Base model"""
+    objects = models.Manager()
+
+    id = models.UUIDField(verbose_name='Идентификатор', primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+
+    class Meta:
+        abstract = True
+
+class Catalog(BaseModel):
+    """Model of catalog"""
+
+    code = models.CharField(verbose_name='Код', max_length=100)
+    name = models.CharField(verbose_name='Наименование', max_length=300)
+    description = models.TextField(verbose_name='Описание', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Справочник'
+        verbose_name_plural = 'Справочники'
+
+class VersionCatalog(BaseModel):
+    """Model of version catalog"""
+
+    catalog = models.ForeignKey(Catalog, verbose_name='Идентификатор справочника', on_delete=models.CASCADE)
+    version = models.CharField(verbose_name='Версия', max_length=50)
+    data_start_actual = models.DateField(verbose_name='Дата начала действия версии', blank=True, null=True)
+
+    def __str__(self):
+        return self.version
+
+    class Meta:
+        verbose_name = 'Версия справочника'
+        verbose_name_plural = 'Версии справочника'
+
+class ElementCatalog(BaseModel):
+    """Model of element catalog"""
+
+    version_catalog = models.ForeignKey(VersionCatalog, verbose_name='Идентификатор версии справочника', on_delete=models.CASCADE)
+    code_element = models.CharField(verbose_name='Код элемента', max_length=100)
+    value_element = models.CharField(verbose_name='Значение элемента', max_length=300)
+
+    def __str__(self):
+        return self.code_element
+
+    class Meta:
+        verbose_name = 'Элемент справочника'
+        verbose_name_plural = 'Элементы справочника'
