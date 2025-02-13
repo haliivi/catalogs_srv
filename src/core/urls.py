a80b4eb05bcf3390788ba0b9b78c237e0e3ee41c
Сотниченко Алексей Дмитриевич <a.sotnichenko@els24.com>
2025-02-13 14:25:47 +0700

    * replace router

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,12 +1,9 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 from .views import *
 
-router = DefaultRouter()
-router.register('refbooks', CatalogViewSet, basename='catalog_list')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('refbooks/', CatalogViewSet.as_view({'get': 'list'}), name='catalog'),
     path('refbooks/<uuid:id_>/elements/', ElementCatalogViewSet.as_view({'get': 'list'}), name='catalog_elements'),
     path('refbooks/<uuid:id_>/check-element/', CheckElementViewSet.as_view({'get': 'check_element'}), name='check_element'),
 ]
